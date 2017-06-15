520dea8bfb1a0cab25344a8dcaaf4d4b5cd4b971
Temesgen Boja <temesgen.dessalegn@gmail.com>
2017-06-15 19:07:18 +0300

    Add index.js

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-const http = require('http');
+var http = require('http'),
+    express = require('express');
 
-const hostName = '127.0.0.1';
-const port = '8080';
+var app = express();
 
-const httpServer = http.createServer((req, res) =>{
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain')
-    res.end('Hello from Nodejs');
+app.get('/', function(req, res) {
+    res.end('Helloo');
 });
 
-httpServer.listen(8080, () => {
-    console.log(`Server is running at http://${hostName}:${port}/`);
-})
+http.createServer(app).listen(8080);
